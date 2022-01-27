3a75d15343a89fa24fceb63839b2e90663e8db96
Donghyeon Park <donpark@c1r8s1.42seoul.kr>
2022-01-27 21:04:46 +0900

    list

--- Linkedlist/main.c
+++ Linkedlist/main.c
@@ -2,45 +2,31 @@
 
 void	func1()
 {
-	LinkedList	*linkedlist;
-	linkedlist = createLinkedList();
-
+	LinkedList	*list;
 	ListNode	node;
-	node.pLink = NULL;
+	int			i;
 
-	node.data = 10;
-	addLLElement(linkedlist, 0, node);
-	displayLinkedList(linkedlist);
+	list = createLinkedList();
+	for (i = 0; i < 5; i++)
+	{
+		node.data = i + 10;
+		addLLElement(list, i, node);
+		displayLinkedList(list);
+	}
 
-	node.data = 11;
-	addLLElement(linkedlist, 1, node);
-	displayLinkedList(linkedlist);
+	removeLLElement(list, 4);
+	displayLinkedList(list);
 
-	node.data = 12;
-	addLLElement(linkedlist, 2, node);
-	displayLinkedList(linkedlist);
+	removeLLElement(list, 0);
+	displayLinkedList(list);
 
-	node.data = 13;
-	addLLElement(linkedlist, 3, node);
-	displayLinkedList(linkedlist);
+	removeLLElement(list, 1);
+	displayLinkedList(list);
 
-	node.data = 14;
-	addLLElement(linkedlist, 4, node);
-	displayLinkedList(linkedlist);
+	clearLinkedList(list);
+	displayLinkedList(list);
 
-	removeLLElement(linkedlist, 4);
-	displayLinkedList(linkedlist);
-
-	removeLLElement(linkedlist, 0);
-	displayLinkedList(linkedlist);
-
-	removeLLElement(linkedlist, 1);
-	displayLinkedList(linkedlist);
-
-	clearLinkedList(linkedlist);
-	displayLinkedList(linkedlist);
-
-	deleteLinkedList(linkedlist);
+	deleteLinkedList(list);
 }
 
 void func2(void)
