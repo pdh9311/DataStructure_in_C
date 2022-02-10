ac670274716b87802037a84a41b98a8344853fa6
Donghyeon Park <donpark@c1r8s5.42seoul.kr>
2022-02-10 12:33:56 +0900

    LinkedDeque

--- 03_Queue/LinkedDeque/linkeddeque.c
+++ 03_Queue/LinkedDeque/linkeddeque.c
@@ -13,11 +13,42 @@ LinkedDeque* createLinkedDeque()
 
 int insertFrontLD(LinkedDeque* pDeque, DequeNode element)
 {
+	DequeNode *addnode;
+	DequeNode *prevFront;
 
+	if (pDeque == NULL) {
+		printf("%screate deque!!!%s\n", C_RED, C_NC);
+		return (FALSE);
+	}
+	addnode = (DequeNode *)malloc(sizeof(DequeNode));
+	if (addnode == NULL)
+		return (FALSE);
+	*addnode = element;
+	prevFront = pDeque->pFrontNode;
+	addnode->pRLink = prevFront;
+	addnode->pLLink = NULL;
+	prevFront->pLLink = addnode;
+	pDeque->pFrontNode = addnode;
+	if (pDeque->currentElementCount == 0)
+		pDeque->pRearNode = addnode;
+	pDeque->currentElementCount++;
 }
 
 int insertRearLD(LinkedDeque* pDeque, DequeNode element)
 {
+	DequeNode *addnode;
+	DequeNode *prevRear;
+
+	addnode = (DequeNode *)malloc(sizeof(DequeNode));
+	if (addnode == NULL)
+		return (FALSE);
+	*addnode = element;
+	prevRear = pDeque->pRearNode;
+	addnode->pLLink = prevRear;
+	addnode->pRLink = NULL;
+	prevRear->pRLink = addnode;
+	pDeque->pRearNode = addnode;
+
 
 }
 
@@ -58,5 +89,17 @@ int isLinkedDequeEmpty(LinkedDeque* pDeque)
 
 void displayLinkedDeque(LinkedDeque* pDeque)
 {
+	DequeNode *tmp;
 
+	if (pDeque == NULL) {
+		printf("create deque!!\n");
+		return ;
+	}
+	tmp = pDeque->pFrontNode;
+	printf("Front ⇄ ");
+	while (tmp != NULL) {
+		printf("%c ", tmp->data);
+		tmp = tmp->pRLink;
+	}
+	printf("⇄ Rear\n");
 }
--- 03_Queue/LinkedDeque/main.c
+++ 03_Queue/LinkedDeque/main.c
@@ -4,7 +4,47 @@ int main(void)
 {
 	LinkedDeque *deque = NULL;
 	DequeNode node;
+	int select;
+	char data;
 
+	while (1) {
+		printf("%s0: EXIT | 1: create | 2: insertFront | 3: insertRear | 4: deleteFront | 5: deleteRear | 6: peekFront | 7: peekRear | 8: delete | 9: display%s\n%s> %s", C_AQUA, C_NC, C_GREEN, C_NC);
+		scanf("%d", &select); fflush(stdin);
+		switch (select) {
+			case 0:
+				exit(0);
+				break ;
+			case 1:
+				if (deque == NULL) {
+					deque = createLinkedDeque();
+				} else {
+					printf("already created deque\n");
+				}
+				break ;
+			case 2:
+				printf("ADD data: ");
+				scanf("%c", &data); fflush(stdin);
+				node.data = data;
+				insertFrontLD(deque, node);
+				break ;
+			case 3:
+				break ;
+			case 4:
+				break ;
+			case 5:
+				break ;
+			case 6:
+				break ;
+			case 7:
+				break ;
+			case 8:
+				break ;
+			case 9:
+				displayLinkedDeque(deque);
+				break ;
+		}
+		printf("\n");
+	}
 
 	return (0);
 }
