6b1c919025a285f88f26e6fd70b891fbd704f5d6
Donghyeon Park <donpark@c1r8s5.42seoul.kr>
2022-02-18 14:24:06 +0900

    bintree

---
+++ 04_Tree/bintree.c
@@ -0,0 +1,33 @@
+#include "bintree.h"
+
+BinTree* makeBinTree(BinTreeNode rootNode) {
+
+}
+
+BinTreeNode* getRootNodeBT(BinTree* pBinTree) {
+
+}
+
+BinTreeNode* insertLeftChildNodeBT(BinTreeNode* pParentNode, BinTreeNode element) {
+
+}
+
+BinTreeNode* insertRightChildNodeBT(BinTreeNode* pParentNode, BinTreeNode element) {
+
+}
+
+BinTreeNode* getLeftChildNodeBT(BinTreeNode* pNode) {
+
+}
+
+BinTreeNode* getRightChildNodeBT(BinTreeNode* pNode) {
+
+}
+
+void deleteBinTree(BinTree* pBinTree) {
+
+}
+
+void deleteBinTreeNode(BinTreeNode* pNode) {
+
+}
