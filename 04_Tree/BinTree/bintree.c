66ba196d79fecb9f112d0d8951cefc149d07d109
Donghyeon Park <donpark@c1r8s3.42seoul.kr>
2022-02-24 14:41:50 +0900

    BinTree

--- 04_Tree/BinTree/bintree.c
+++ 04_Tree/BinTree/bintree.c
@@ -26,34 +26,30 @@ BinTreeNode* getRootNodeBT(BinTree* pBinTree) {
 
 BinTreeNode* insertLeftChildNodeBT(BinTreeNode* pParentNode, BinTreeNode element) {
 	BinTreeNode *addNode;
-	BinTreeNode *tmp;
 
-	if (pParentNode == NULL)
+	if (pParentNode == NULL || pParentNode->pLeftChild != NULL)
 		return (NULL);
 	addNode = (BinTreeNode *)malloc(sizeof(BinTreeNode));
 	if (addNode == NULL)
 		return (NULL);
 	addNode->data = element.data;
+	addNode->pLeftChild = NULL;	
 	addNode->pRightChild = NULL;
-	tmp = pParentNode->pLeftChild;
-	addNode->pLeftChild = tmp;
 	pParentNode->pLeftChild = addNode;
 	return (addNode);
 }
 
 BinTreeNode* insertRightChildNodeBT(BinTreeNode* pParentNode, BinTreeNode element) {
 	BinTreeNode *addNode;
-	BinTreeNode *tmp;
 
-	if (pParentNode == NULL)
+	if (pParentNode == NULL || pParentNode->pRightChild != NULL)
 		return (NULL);
 	addNode = (BinTreeNode *)malloc(sizeof(BinTreeNode));
 	if (addNode == NULL)
 		return (NULL);
 	addNode->data = element.data;
 	addNode->pLeftChild = NULL;
-	tmp = pParentNode->pRightChild;
-	addNode->pRightChild = tmp;
+	addNode->pRightChild = NULL;
 	pParentNode->pRightChild = addNode;
 	return (addNode);
 }
@@ -76,51 +72,33 @@ void deleteBinTree(BinTree* pBinTree) {
 }
 
 void deleteBinTreeNode(BinTreeNode* pNode) {
-	if(pNode->pLeftChild == NULL && pNode->pRightChild == NULL) {
-		free(pNode);
+	if(pNode == NULL )
 		return ;
-	}
-	if (pNode->pLeftChild != NULL)
-		deleteBinTreeNode(pNode->pLeftChild);
-	if (pNode->pRightChild != NULL)
-		deleteBinTreeNode(pNode->pRightChild);
-	pNode->pLeftChild = NULL;
-	pNode->pRightChild = NULL;
+	deleteBinTreeNode(pNode->pLeftChild);
+	deleteBinTreeNode(pNode->pRightChild);
 	free(pNode);
 }
 
 void preorderTraversal(BinTreeNode *pNode) {
-	if (pNode->pLeftChild == NULL && pNode->pRightChild == NULL) {
-		printf("%c ", pNode->data);
+	if (pNode == NULL)
 		return ;
-	}
 	printf("%c ", pNode->data);
-	if (pNode->pLeftChild != NULL)
-		preorderTraversal(pNode->pLeftChild);
-	if (pNode->pRightChild != NULL)
-		preorderTraversal(pNode->pRightChild);
+	preorderTraversal(pNode->pLeftChild);
+	preorderTraversal(pNode->pRightChild);
 }
 
 void inorderTraversal(BinTreeNode *pNode) {
-	if (pNode->pLeftChild == NULL && pNode->pRightChild == NULL) {
-		printf("%c ", pNode->data);
+	if (pNode == NULL)
 		return ;
-	}
-	if (pNode->pLeftChild != NULL)
-		inorderTraversal(pNode->pLeftChild);
+	inorderTraversal(pNode->pLeftChild);
 	printf("%c ", pNode->data);
-	if (pNode->pRightChild != NULL)
-		inorderTraversal(pNode->pRightChild);
+	inorderTraversal(pNode->pRightChild);
 }
 
 void postorderTraversal(BinTreeNode *pNode) {
-	if (pNode->pLeftChild == NULL && pNode->pRightChild == NULL) {
-		printf("%c ", pNode->data);
+	if (pNode == NULL)
 		return ;
-	}
-	if (pNode->pLeftChild != NULL)
-		postorderTraversal(pNode->pLeftChild);
-	if (pNode->pRightChild != NULL)
-		postorderTraversal(pNode->pRightChild);
+	postorderTraversal(pNode->pLeftChild);
+	postorderTraversal(pNode->pRightChild);
 	printf("%c ", pNode->data);
 }
