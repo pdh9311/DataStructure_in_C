f0e1d5bcbd0f0490ab165eba8b896a6d103b77f1
Donghyeon Park <donpark@c8r1s4.42seoul.kr>
2022-03-15 12:23:11 +0900

    sort

--- 06_Sort/Bubble/bubble.c
+++ 06_Sort/Bubble/bubble.c
@@ -6,14 +6,33 @@ void bubbleSort(ArrayList *arrayList)
 	int i, j;
 	int tmp;
 
-	for (i = size - 1; i >= 1; i--) {
-		for (j = 0; j < i; j++) {
-			if ((arrayList->pElement)[j + 1].data < (arrayList->pElement)[j].data) {
+	/*
+		94 40 39 24 4
+
+		40 94 39 24 4
+		40 39 94 24 4
+		40 39 24 94 4
+		40 39 24 4  |94
+
+		39 40 24 4  |94
+		39 24 40 4  |94
+		39 24 4  |40 94
+
+		24 39 4  |40 94
+		24 4  |39 40 94
+
+		4 24 39 40 94
+	 */
+	for (i = size - 1; i >= 1; i--)
+	{
+		for (j = 0; j < i; j++)	// j == size - 2
+		{
+			if ((arrayList->pElement)[j + 1].data < (arrayList->pElement)[j].data)
+			{
 				tmp = (arrayList->pElement)[j + 1].data;
 				(arrayList->pElement)[j + 1].data = (arrayList->pElement)[j].data;
 				(arrayList->pElement)[j].data = tmp;
 			}
 		}
 	}
-
 }
--- 06_Sort/Bubble/main.c
+++ 06_Sort/Bubble/main.c
@@ -9,7 +9,7 @@ int main(void)
 
 	srand(time(NULL));
 	arrayList = createArrayList(max);
-	for (int i = 0; i < max; i++) {
+	for (int i = 0; i < 5; i++) {
 		element.data = rand() % 100;	// 0 ~ 99
 		addALElement(arrayList, i, element);
 	}
@@ -18,15 +18,15 @@ int main(void)
 
 	printf("========= ascending sort =========\n");
 	bubbleSort(arrayList);
-	displayArrayList(arrayList); printf("\n");
+	// displayArrayList(arrayList); printf("\n");
 
-	printf("========= descending sort =========\n");
-	for (int i = 0; i < arrayList->currentElementCount; i++) {
-		(arrayList->pElement)[i].data *= -1;
-	}
-	bubbleSort(arrayList);
-	for (int i = 0; i < arrayList->currentElementCount; i++) {
-		(arrayList->pElement)[i].data *= -1;
-	}
-	displayArrayList(arrayList); printf("\n");
+	// printf("========= descending sort =========\n");
+	// for (int i = 0; i < arrayList->currentElementCount; i++) {
+	// 	(arrayList->pElement)[i].data *= -1;
+	// }
+	// bubbleSort(arrayList);
+	// for (int i = 0; i < arrayList->currentElementCount; i++) {
+	// 	(arrayList->pElement)[i].data *= -1;
+	// }
+	// displayArrayList(arrayList); printf("\n");
 }
